4ae3eb96eaf6b93c57eec3d9b27e0c2cd89f4323
Daniel Schaffer <dschaffer@instawork.com>
2022-09-21 13:27:21 -0400

    feat: add --reset argument for git-parent-branch to allow recalculating the parent branch based on the log

--- git-parent-branch.fish
+++ git-parent-branch.fish
@@ -1,7 +1,12 @@
-function git-parent-branch --description 'determines the parent branch of the current branch, if any, using git log'
+function git-parent-branch --argument-names reset_flag --description 'determines the parent branch of the current branch, if any, using git log'
 
   set current_branch (git-curbranch)
-  set parent_branch_from_config (git config --get "branch.$current_branch.parent")
+
+  if [ "$reset_flag" = "--reset" ]
+    git config --unset "branch.$current_branch.parent"
+  else
+    set parent_branch_from_config (git config --get "branch.$current_branch.parent")
+  end
 
   if test $parent_branch_from_config
     echo $parent_branch_from_config
