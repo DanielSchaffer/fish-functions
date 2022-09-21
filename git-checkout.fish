ff2138dd85a4bc0b9cd51a4d562e524295cc619a
Daniel Schaffer <dschaffer@instawork.com>
2022-09-21 12:22:57 -0400

    feat: track "parent" branch and add helpers for working with nested branches

---
+++ git-checkout.fish
@@ -0,0 +1,51 @@
+# Defined in - @ line 1
+function git-checkout --wraps='git branch' --description 'returns the current branch' --argument-names branch_flag branch
+
+  if [ $branch_flag = "-b" ]
+    set parent (git branch --show-current)
+    git checkout -b $branch
+    git config --local --add "branch.$branch.parent" $parent
+    return 0
+  end
+
+  if [ $branch_flag = "--prev" ]
+    set cur_branch (git-curbranch)
+    set parent (git config --get "branch.$cur_branch.parent")
+
+    if not test $parent
+      echo "No parent configured for $cur_branch"
+      return -1
+    end
+
+    git checkout $parent
+
+    return 0
+  end
+
+  if [ $branch_flag = "--next" ]
+    set cur_branch (git-curbranch)
+    set children (git config --name-only --get-regexp ^branch\..+\.parent\$ $cur_branch)
+
+    if test (count $children) -eq 0
+      echo "No children configured for $cur_branch"
+      return -1
+    end
+
+    if test (count $children) -gt 1
+      echo "Multiple children found:"
+      for child in children
+        echo "  $child"
+      end
+      return -1
+    end
+
+    set child_branch $children
+    set child_branch (string replace --regex ^branch\. "" $children)
+    set child_branch (string replace --regex \.parent\$ "" $child_branch)
+
+    git checkout $child_branch
+    return 0
+  end
+
+  git checkout $branch_flag
+end
--- git-parent-branch.fish
+++ git-parent-branch.fish
@@ -1,11 +1,22 @@
 function git-parent-branch --description 'determines the parent branch of the current branch, if any, using git log'
 
+  set current_branch (git-curbranch)
+  set parent_branch_from_config (git config --get "branch.$current_branch.parent")
+
+  if test $parent_branch_from_config
+    echo $parent_branch_from_config
+    return
+  end
+
   set parent_branches (git log master..HEAD~1 --format="%d" | grep "(" | awk -v RS=',' '{gsub(/[() ]/, ""); print $1 }' | grep -v "origin/")
 
   if test $parent_branches[1]
-    echo $parent_branches[1]
-  else
-    echo "master"
+    set parent $parent_branches[1]
+    git config --local --add "branch.$current_branch.parent" $parent
+    echo $parent
+    return
   end
+
+  echo "master"
 
 end
