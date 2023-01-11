be5ec35c5cb8fde915ae264d526e194b94a9e21b
Daniel Schaffer <dschaffer@instawork.com>
2023-01-11 13:58:50 -0500

    feat(git-sync): add --auto option for recursively synchronizing child branches

--- git-sync.fish
+++ git-sync.fish
@@ -1,12 +1,19 @@
 # Defined in - @ line 1
-function git-sync --description 'git push, setting origin if it is not already set'
+function git-sync --description 'git push, setting origin if it is not already set' --argument-names auto
   set cur_branch (git-curbranch);
-  set target_branch $argv
-  string length -q -- $target_branch; or set target_branch "master"
+  string length -q -- $target_branch; or set target_branch (git-parent-branch)
   echo Syncing $cur_branch to $target_branch...
 
   git checkout $target_branch
   git pull
   git checkout $cur_branch
   git rebase $target_branch
+
+  if [ "$auto" = "--auto" ]
+    git-checkout --next
+
+    while test $status -eq 0
+      git rebase (git-parent-branch) && git push -f && git-checkout --next
+    end
+  end
 end
