3b084a0643e5755fc246f3d33caabb324daa07e3
Daniel Schaffer <dschaffer@instawork.com>
2023-06-27 09:31:31 -0400

    feat(git-done): improve UX when the "done" branch has a child branch
    instead of moving to master, check out the child branch and rebase it to master

--- git-done.fish
+++ git-done.fish
@@ -2,6 +2,17 @@
 function git-done --description 'removes the current branch and returns to the parent or default branch'
 
   set branch (git-curbranch)
+
+  if [ (git-parent-branch) = "master" ]
+    set child_branch (git-child-branch)
+    if test $child_branch
+      git checkout $child_branch
+      git rebase master
+      git-parent-branch --reset
+      return 0
+    end
+  end
+
   git-checkout --prev
   git branch -D $branch
 
